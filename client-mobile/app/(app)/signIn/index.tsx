adf4d21e59b83e000eb4a4812ffa883a13f1e075
Baranin67 <mati.baranowicz@gmail.com>
2025-03-31 00:46:18 +0200

    client: add auth-related screens

---
+++ client-mobile/app/(app)/signIn/index.tsx
@@ -0,0 +1,10 @@
+import Text from '@/components/ui/Text';
+import { View } from 'react-native';
+
+export default function SignInScreen() {
+	return (
+		<View>
+			<Text>Sign in screen</Text>
+		</View>
+	);
+}
---
+++ client-mobile/app/(app)/signUp/index.tsx
@@ -0,0 +1,10 @@
+import Text from '@/components/ui/Text';
+import { View } from 'react-native';
+
+export default function SignUpScreen() {
+	return (
+		<View>
+			<Text>Sign up screen</Text>
+		</View>
+	);
+}

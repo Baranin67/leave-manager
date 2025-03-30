13a54e68bf2f8ebcaf2f46d3d587f17a144ea5f7
Baranin67 <mati.baranowicz@gmail.com>
2025-03-31 00:43:19 +0200

    client: add `Colors` constant

--- client-mobile/constants/Colors.ts
+++ client-mobile/constants/Colors.ts
@@ -1,39 +1,39 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
+import { ColorScheme } from '@/hooks/useColorScheme.web';
 
-import { ColorSchemeName } from 'react-native';
-
-type ShadedColor = {
-	[key: number]: string;
+type Colors = {
+	primary: [100, 60, 40, 20, 10, 5];
+	secondary: [100];
+	secondaryDark: [100];
+	inputPlaceholder: [100];
+	inputIcon: [100];
+	inputLabel: [100];
+	green: [100, 40, 20, 10];
+	red: [100, 60, 40, 20, 10];
+	yellow: [100, 40, 20, 10];
+	blue: [100, 40, 20, 10];
+	text: [100, 80, 60, 20, 10];
+	textInverted: [100, 60];
+	background: [100];
+	backgroundLighter: [100, 60];
 };
 
-type Color = string;
+type Shades<Color extends keyof Colors> = Colors[Color][number];
 
-type ColorsType = {
-	[Scheme in ColorSchemeName]: {
-		primary: ShadedColor;
-		secondary: ShadedColor;
-		secondaryDark: ShadedColor;
-		inputPlaceholder: Color;
-		inputIcon: Color;
-		inputLabel: Color;
-		green: ShadedColor;
-		red: ShadedColor;
-		yellow: ShadedColor;
-		blue: ShadedColor;
-		text: ShadedColor;
-		textInverted: ShadedColor;
-		background: Color;
-		backgroundLighter: Color;
+type Scheme = {
+	[Color in keyof Colors]: {
+		[Shade in Shades<Color>]: string;
 	};
+};
+
+type Schemes = {
+	[_ in ColorScheme]: Scheme;
 };
 
 export const Colors = {
 	light: {
 		primary: {
 			100: 'hsla(251, 52%, 13%, 1)',
+			60: 'hsla(251, 52%, 13%, .6)',
 			40: 'hsla(251, 52%, 13%, .4)',
 			20: 'hsla(251, 52%, 13%, .2)',
 			10: 'hsla(251, 52%, 13%, .1)',
@@ -45,13 +45,20 @@ export const Colors = {
 		secondaryDark: {
 			100: 'hsla(251, 35%, 81%, 1)',
 		},
-		inputPlaceholder: 'hsla(251, 31%, 79%, 1)',
-		inputIcon: 'hsla(251, 25%, 47%, 1)',
-		inputLabel: 'hsla(251, 25%, 47%, 1)',
+		inputPlaceholder: {
+			100: 'hsla(251, 31%, 79%, 1)',
+		},
+		inputIcon: {
+			100: 'hsla(251, 25%, 47%, 1)',
+		},
+		inputLabel: {
+			100: 'hsla(251, 25%, 47%, 1)',
+		},
 		green: {
 			100: 'hsla(145, 63%, 49%, 1)',
 			40: 'hsla(145, 63%, 49%, .4)',
 			20: 'hsla(145, 63%, 49%, .2)',
+			10: 'hsla(145, 63%, 49%, .1)',
 		},
 		red: {
 			100: 'hsla(6, 78%, 57%, 1)',
@@ -64,25 +71,37 @@ export const Colors = {
 			100: 'hsla(48, 89%, 50%, 1)',
 			40: 'hsla(48, 89%, 50%, .4)',
 			20: 'hsla(48, 89%, 50%, .2)',
+			10: 'hsla(48, 89%, 50%, .1)',
 		},
 		blue: {
 			100: 'hsla(197, 63%, 49%, 1)',
 			40: 'hsla(197, 63%, 49%, .4)',
+			20: 'hsla(197, 63%, 49%, .2)',
+			10: 'hsla(197, 63%, 49%, .1)',
 		},
 		text: {
 			100: 'hsla(0, 0%, 7%, 1)',
+			80: 'hsla(0, 0%, 7%, .8)',
+			60: 'hsla(0, 0%, 7%, .6)',
+			20: 'hsla(0, 0%, 7%, .2)',
+			10: 'hsla(0, 0%, 7%, .1)',
 		},
 		textInverted: {
 			100: 'hsla(0, 0%, 93%, 1)',
 			60: 'hsla(0, 0%, 93%, .6)',
 		},
-		background: 'hsla(0, 0%, 93%, 1)',
-		backgroundLighter: 'hsla(0, 0%, 100%, 1)',
+		background: {
+			100: 'hsla(0, 0%, 93%, 1)',
+		},
+		backgroundLighter: {
+			100: 'hsla(0, 0%, 100%, 1)',
+			60: 'hsla(0, 0%, 100%, .6)',
+		},
 	},
 	dark: {
-		// TODO
 		primary: {
 			100: 'hsla(251, 52%, 13%, 1)',
+			60: 'hsla(251, 52%, 13%, .6)',
 			40: 'hsla(251, 52%, 13%, .4)',
 			20: 'hsla(251, 52%, 13%, .2)',
 			10: 'hsla(251, 52%, 13%, .1)',
@@ -94,13 +113,20 @@ export const Colors = {
 		secondaryDark: {
 			100: 'hsla(251, 35%, 81%, 1)',
 		},
-		inputPlaceholder: 'hsla(251, 31%, 79%, 1)',
-		inputIcon: 'hsla(251, 25%, 47%, 1)',
-		inputLabel: 'hsla(251, 25%, 47%, 1)',
+		inputPlaceholder: {
+			100: 'hsla(251, 31%, 79%, 1)',
+		},
+		inputIcon: {
+			100: 'hsla(251, 25%, 47%, 1)',
+		},
+		inputLabel: {
+			100: 'hsla(251, 25%, 47%, 1)',
+		},
 		green: {
 			100: 'hsla(145, 63%, 49%, 1)',
 			40: 'hsla(145, 63%, 49%, .4)',
 			20: 'hsla(145, 63%, 49%, .2)',
+			10: 'hsla(145, 63%, 49%, .1)',
 		},
 		red: {
 			100: 'hsla(6, 78%, 57%, 1)',
@@ -113,20 +139,31 @@ export const Colors = {
 			100: 'hsla(48, 89%, 50%, 1)',
 			40: 'hsla(48, 89%, 50%, .4)',
 			20: 'hsla(48, 89%, 50%, .2)',
+			10: 'hsla(48, 89%, 50%, .1)',
 		},
 		blue: {
 			100: 'hsla(197, 63%, 49%, 1)',
 			40: 'hsla(197, 63%, 49%, .4)',
+			20: 'hsla(197, 63%, 49%, .2)',
+			10: 'hsla(197, 63%, 49%, .1)',
 		},
 		text: {
 			100: 'hsla(0, 0%, 7%, 1)',
+			80: 'hsla(0, 0%, 7%, .8)',
+			60: 'hsla(0, 0%, 7%, .6)',
+			20: 'hsla(0, 0%, 7%, .2)',
+			10: 'hsla(0, 0%, 7%, .1)',
 		},
 		textInverted: {
 			100: 'hsla(0, 0%, 93%, 1)',
 			60: 'hsla(0, 0%, 93%, .6)',
-			10: 'hsla(0, 0%, 93%, .1)',
 		},
-		background: 'hsla(0, 0%, 93%, 1)',
-		backgroundLighter: 'hsla(0, 0%, 100%, 1)',
+		background: {
+			100: 'hsla(0, 0%, 93%, 1)',
+		},
+		backgroundLighter: {
+			100: 'hsla(0, 0%, 100%, 1)',
+			60: 'hsla(0, 0%, 100%, .6)',
+		},
 	},
-} as const satisfies ColorsType;
+} as const satisfies Schemes;

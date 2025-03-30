84009c5bf05711b462b8770e6893a5056ad62b46
Baranin67 <mati.baranowicz@gmail.com>
2025-03-31 00:42:47 +0200

    client: update `useColorScheme`

--- client-mobile/hooks/useColorScheme.web.ts
+++ client-mobile/hooks/useColorScheme.web.ts
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useColorScheme as useRNColorScheme } from 'react-native';
 
+export type ColorScheme = 'light' | 'dark';
+
 /**
  * To support static rendering, this value needs to be re-calculated on the client side for web
  */
-export function useColorScheme() {
+export function useColorScheme(): ColorScheme {
 	const [hasHydrated, setHasHydrated] = useState(false);
 
 	useEffect(() => {
